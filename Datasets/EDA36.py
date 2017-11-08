bc8478a2bdc118457d90030d0870b41695d2be4d
Eddy Ekofo <contact@eddyekofo.com>
2017-11-08 12:00:15 +0000

    Finial project

--- Datasets/EDA36.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-with open('EDA36-Percentage_of_persons.json') as json_data:
-    DATA = json.load(json_data)
-    VALUE = DATA["dataset"]["value"]
-
-    LABEL = DATA["dataset"]["dimension"]["Sex"]
-    print(LABEL)
